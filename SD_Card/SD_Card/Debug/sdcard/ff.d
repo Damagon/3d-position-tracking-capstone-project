005b63602dd089c318d14ced4eaa49b996aca1c1
IE99 <46275517+IE99@users.noreply.github.com>
2022-03-16 17:28:53 -0600

    tried something

---
+++ SD_Card/SD_Card/Debug/sdcard/ff.d
@@ -0,0 +1,10 @@
+sdcard/ff.d sdcard/ff.o: ../sdcard/ff.cpp ../sdcard/ff.h \
+ ../sdcard/integer.h ../sdcard/ffconf.h ../sdcard/diskio.h
+
+../sdcard/ff.h:
+
+../sdcard/integer.h:
+
+../sdcard/ffconf.h:
+
+../sdcard/diskio.h:
