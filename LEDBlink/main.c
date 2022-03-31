695539f876f21cf3e0664442577f3e045a7317f0
Damagon <30881380+Damagon@users.noreply.github.com>
2022-03-30 20:43:23 -0600

    eeprom and button code added -
    test imu code commented out

--- LEDBlink/main.c
+++ LEDBlink/main.c
@@ -12,11 +12,20 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) {
 	
-	DDRD |= (1 << PD0);			// set Port D pin PD0 for output
+	DDRD |= (1 << PD0);	
+	DDRD |= (1 << PD1);
+	DDRB &= ~(1<<PB0);	//set button as 
+	PORTB|= (1<<PB0);		// set Port D pin PD0 for output
 	
-	while (1) {					// begin infinite loop
-		PORTD ^= (1 << PD0);	// flip state of LED on PD0
-		_delay_ms(500);			// delay 1/2 second
+	while(1){
+	if(!(PINB & (1<<PB0))) {					// begin infinite loop
+		PORTD |= (1 << PD1);
+		}else{	
+		
+		PORTD &= ~(1<<PD1);					// flip state of LED on PD0
+	
+		}
+	
 	}
 	return(0);					// should never get here, this is to prevent a compiler warning
 }
